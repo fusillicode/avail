cf62cbab5fe34813359f50f024e6fd014b78c06e
fusillicode <fusillicode@gmail.com>
2017-01-28 11:11:00 +0100

    move availabilities logic outside Event model

--- app/models/availabilities.rb
+++ app/models/availabilities.rb
@@ -1,5 +1,6 @@
-# FIXME: this is just a draft created from `Events.availabilities`
 class Availabilities
+  TIME_SLOT_SIZE = 30.minutes
+
   attr_reader :from, :to
 
   def initialize(from, to = nil)
@@ -8,11 +9,14 @@ class Availabilities
   end
 
   def get
-    appointments = events_of_interest.select { |event| event.kind == 'appointment' }
-    openings.map do |opening_day, opening_times|
+    events       = Event.for_availbilites_calculation from, to, week_days_of_temporal_frame
+    appointments = events.select { |event| event.kind == 'appointment' }
+    openings_from_events(events).map do |opening_day, opening_time_slots|
       {
         date:  opening_day,
-        slots: availabilities(squashed_opening_times(opening_times), appointments)
+        slots: format_available_time_slots(
+          available_time_slots(opening_day, opening_time_slots, appointments)
+        )
       }
     end
   end
@@ -23,65 +27,55 @@ class Availabilities
     (from..to).to_a
   end
 
-  def temporal_frame_week_days
+  def week_days_of_temporal_frame
     temporal_frame.group_by(&:wday).keys.map &:to_s
   end
 
-  # Merging adiacent opening_times to build NON adiacent ones
-  def squashed_opening_times(opening_times)
-    opening_times = opening_times.to_a
-    opening_times.size.odd? ? [opening_times.first, opening_times.last] : opening_times
+  def openings_from_events(events)
+    # Prepare the temporal frame for the openings
+    openings = Hash[temporal_frame.map { |day| [day, SortedSet.new] }]
+    # Populate the temporal frame with the normal openings together with the
+    # weekly recurring "actualized"
+    events.select { |event| event.kind == 'opening' }.each do |opening|
+      if opening.weekly_recurring
+        temporal_frame.select { |date| date.wday == opening.starts_at.wday }.each do |actualized_opening_date|
+          openings[actualized_opening_date] << time_slots_for_event(opening)
+        end
+      else
+        openings[opening.starts_at] << time_slots_for_event(opening)
+      end
+      openings
+    end
+    openings
   end
 
-  # To work correctly the appointments should be ordered by starts_at!
-  def availabilities(squashed_opening_times, appointments)
-    squashed_opening_times.each_slice(2).inject([]) do |memo, opening|
-      last_availability_bound = opening.first
+  def available_time_slots(opening_day, opening_time_slots, appointments)
+    opening_time_slots.inject(SortedSet.new) do |memo, opening_time_slots|
+      memo = opening_time_slots
+
       appointments.each do |appointment|
-        if appointment.starts_at.to_date != opening_day.to_date && last_availability_bound != opening.last
-          memo += format_slot([last_availability_bound, opening.last])
-          break
-        elsif last_availability_bound != (appointment_start = appointment.starts_at)
-          memo += format_slot([last_availability_bound, appointment_start])
-        end
-        last_availability_bound = appointment.ends_at
+        break if appointment.starts_at.to_date != opening_day.to_date
+        appointment_time_slots = SortedSet.new time_slots_for_event(appointment)
+        memo -= appointment_time_slots
+        appointments.delete appointment
       end
-      memo += format_slot([last_availability_bound, opening.last]) if last_availability_bound != opening.last
+
       memo
     end
   end
 
-  def openings
-    openings = Hash[temporal_frame.map { |day| [day, SortedSet.new] }]
-
-    # Populute the temporal frame with the normal openings together with the
-    # weekly recurring "actualized"
-    events_of_interest.select { |event| event.kind == 'opening' }.each do |opening|
-      if opening.weekly_recurring
-        temporal_frame.select { |date| date.wday == opening.starts_at.wday }.each do |actualized_opening_date|
-          openings[actualized_opening_date] = openings[actualized_opening_date].merge [opening.starts_at, opening.ends_at]
-        end
-      else
-        openings[opening.starts_at] = openings[opening.starts_at].merge [opening.starts_at, opening.ends_at]
-      end
-      openings
-    end
+  def time_slots_for_event(event)
+    SortedSet.new((event.starts_at.to_i..event.ends_at.to_i).step(TIME_SLOT_SIZE).map do |unix_time|
+      time = Time.at(unix_time).utc
+      [time.hour, time.min]
+    end.tap(&:pop))
   end
 
-  def events_of_interest
-    Event.where("#{events_of_interest_not_weekly_recurring} OR #{events_of_interest_weekly_recurring}",
-                from: from, to: to, week_days: temporal_frame_week_days).order :starts_at
+  def format_available_time_slots(availabilities)
+    availabilities.to_a.map { |time_slot| format_time_slot(time_slot) }
   end
 
-  def events_of_interest_not_weekly_recurring
-    "((weekly_recurring IS NULL OR weekly_recurring = 'f') AND starts_at BETWEEN :from AND :to)"
-  end
-
-  def events_of_interest_weekly_recurring
-    "(weekly_recurring == 't' AND starts_at <= :to AND strftime('%w', starts_at) IN (:week_days))"
-  end
-
-  def format_slot(slot)
-    slot.map { |bound| bound.strftime('%-l:%M') }
+  def format_time_slot(time)
+    "#{time.first}:" + time.last.to_s.ljust(2, '0')
   end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -2,61 +2,20 @@ class Event < ActiveRecord::Base
   TIME_SLOT_SIZE = 30.minutes
 
   def self.availabilities(from_day)
-    to_day         = from_day + 6.days
-    temporal_frame = (from_day..to_day).to_a
-    week_days      = temporal_frame.group_by(&:wday).keys.map &:to_s
-
-    events_of_interest = where("
-      ((weekly_recurring IS NULL OR weekly_recurring = 'f') AND starts_at BETWEEN :from AND :to)
-      OR
-      (weekly_recurring == 't' AND starts_at <= :to AND strftime('%w', starts_at) IN (:week_days))",
-                               from: from_day, to: to_day, week_days: week_days).order :starts_at
-
-    # Prepare the temporal frame for the openings
-    openings = Hash[temporal_frame.map { |day| [day, SortedSet.new] }]
-
-    # Populute the temporal frame with the normal openings together with the
-    # weekly recurring "actualized"
-    events_of_interest.select { |event| event.kind == 'opening' }.each do |opening|
-      if opening.weekly_recurring
-        temporal_frame.select { |date| date.wday == opening.starts_at.wday }.each do |actualized_opening_date|
-          openings[actualized_opening_date] << time_slots_for_event(opening)
-        end
-      else
-        openings[opening.starts_at] << time_slots_for_event(opening)
-      end
-      openings
-    end
-
-    appointments = events_of_interest.select { |event| event.kind == 'appointment' }
-
-    openings.map do |opening_day, opening_time_slots|
-
-      availabilities = opening_time_slots.inject(SortedSet.new) do |memo, opening_time_slots|
-        memo = opening_time_slots
-
-        appointments.each do |appointment|
-          break if appointment.starts_at.to_date != opening_day.to_date
-          appointment_time_slots = SortedSet.new self.time_slots_for_event(appointment)
-          memo = memo - appointment_time_slots
-          appointments.delete appointment
-        end
-
-        memo
-      end.to_a.map { |time_slot| format_time_slot(time_slot) }
-
-      { date: opening_day, slots: availabilities }
-    end
+    Availabilities.new(from_day).get
   end
 
-  def self.time_slots_for_event event
-    SortedSet.new((event.starts_at.to_i..event.ends_at.to_i).step(self::TIME_SLOT_SIZE).map do |unix_time|
-      time = Time.at(unix_time).utc
-      [time.hour, time.min]
-    end.tap { |o| o.pop })
+  def self.for_availbilites_calculation(from, to, week_days)
+    where("#{not_weekly_recurring_condition} OR #{weekly_recurring_condition}",
+          from: from, to: to, week_days: week_days)
+      .order :starts_at
   end
 
-  def self.format_time_slot time
-    "#{time.first}:" + "#{time.last}".ljust(2, '0')
+  def self.not_weekly_recurring_condition
+    "((weekly_recurring IS NULL OR weekly_recurring = 'f') AND starts_at BETWEEN :from AND :to)"
+  end
+
+  def self.weekly_recurring_condition
+    "(weekly_recurring == 't' AND starts_at <= :to AND strftime('%w', starts_at) IN (:week_days))"
   end
 end
